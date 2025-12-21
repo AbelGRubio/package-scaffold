8690ffa8016b234c173aed7faf5975136c26e1d9
Abel <algb@gft.com>
2025-12-21 17:47:58 +0100

    fix: change package scaffold configuracion

--- {{ cookiecutter.folder_name }}/src/{{ cookiecutter.project_slug }}/configuration.py
+++ {{ cookiecutter.folder_name }}/src/{{ cookiecutter.project_slug }}/configuration.py
@@ -1,20 +1,5 @@
 {% if cookiecutter.use_config_file.lower() == "yes" %}import configparser{% endif %}
 import os
-
-__version__ = {{ cookiecutter.version }}
-
-{% if cookiecutter.use_config_file.lower() == "yes" %}
-conf_file = os.getenv("CONF_FILE", "./conf/config.cfg")
-
-config = configparser.ConfigParser()
-config.read(conf_file)
-
-basic_ = config.get("conf", "basic", fallback="")
-{% endif %}
-
-
-{% if cookiecutter.use_config_file.lower() == "yes" %}import configparser{% endif %}
-import os
 from typing import List
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
